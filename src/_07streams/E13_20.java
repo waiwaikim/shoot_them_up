ce67f9523ee5b4ea19d50f909f40fbd8b6ef6b87
Waiwai <waiwai.j.kim@gmail.com>
2019-11-17 13:19:29 -0600

    third commit to HW1.

---
+++ src/_07streams/E13_20.java
@@ -0,0 +1,72 @@
+package _07streams;
+
+import com.sun.javafx.image.IntPixelGetter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class E13_20 {
+    //Given an integer price, list all possible ways of paying for it with $100, $20, $5, and $1 bills, using recursion. Don’t list duplicates
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> bills = new ArrayList<Integer>();
+        bills.add(1);
+        bills.add(5);
+        bills.add(20);
+        bills.add(100);
+        int targetPrice = 10;
+        
+        System.out.println(find_changes(targetPrice, bills).toString());
+    }
+
+    static ArrayList<ArrayList<Integer>> find_changes(int price, ArrayList<Integer> bills ){
+
+        //if(price <= 0) return new ArrayList<ArrayList<Integer>>();
+
+
+        if(price ==  0) {
+
+            ArrayList<ArrayList<Integer>> return_list = new ArrayList<ArrayList<Integer>>();
+            return_list.add(new ArrayList<Integer>());
+            return return_list;
+        }
+        ArrayList<ArrayList<Integer>> myAnswer = new ArrayList<ArrayList<Integer>>();
+
+        for(int bill : bills){
+            if(price-bill>= 0){
+                ArrayList<ArrayList<Integer>> combos = find_changes(price - bill, bills);
+                for (ArrayList<Integer> combo : combos){
+                    combo.add(bill);
+                    Collections.sort(combo);
+                    if(!myAnswer.contains(combo)) myAnswer.add(combo);
+                }
+            }
+        }
+        return myAnswer;
+    }
+
+    /*
+    static void change_recursive(ArrayList<Integer> bills, int price, ArrayList<Integer> partial){
+        int s = 0;
+        for(int x : partial){
+            s += x ;
+        }
+
+        if(s == price) System.out.println("a solution: " + partial.toString());
+
+        if(s >= price) return;
+
+        for(int i=0 ; i<bills.size() ; i++){
+
+            int n = bills.get(i);
+            ArrayList<Integer> partial_rec = new ArrayList<Integer>(partial);
+            partial_rec.add(n);
+            change_recursive(bills, price, partial_rec);
+        }
+    }
+    *
+     */
+
+}
